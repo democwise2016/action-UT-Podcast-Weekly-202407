a1b92e53a87bdb876edc6285eeb44fbcaed31435
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-03 16:40:19 +0800

    西元2024年08月03日 (週六) 16時40分19秒 CST

---
+++ config-json.js
@@ -0,0 +1,20 @@
+let CONFIG = {
+  publicURL: `https://democwise2016.github.io/action-UT-Podcast-Weekly-202407/`,
+  publicURLShorten: `https://rb.gy/hg180y`,
+  thumbnailBorderColor: 'F2D398',
+
+  titleAppend: '',
+  titleSuffix: ' [UT]',
+  idAppend: false,
+
+  maxDownloadItemPerFeed: 50,
+  maxDownloadFeed: 50,
+  newArrialMax: 50,
+  maxDownloadItems: 100,
+  maxExcutionMinutes: 25,
+
+  maxFileCache: 5000,
+  // feedList
+}
+
+module.exports = CONFIG
--- config.js
+++ config.js
@@ -1,16 +1,11 @@
 // const ItemFiltersPreset = require('./app/config/ItemFiltersPreset.js')
 const ConfigCheck = require('./app/config/ConfigCheck.js')
 const feedList = require('./config-list.js')
+const ConfigJSON = require('./config-json.js')
 
 let CONFIG = {
-  publicURL: `https://democwise2016.github.io/action-UT-Podcast-Weekly-202407/`,
-  publicURLShorten: `https://rb.gy/hg180y`,
-  thumbnailBorderColor: 'F2D398',
-  maxDownloadItemPerFeed: 5,
-  maxDownloadFeed: 30,
-  newArrialMax: 5,
-  maxDownloadItems: 100,
-  maxExcutionMinutes: 25,
+  ...ConfigJSON,
+
   feedList
 }
 
